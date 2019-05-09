d4cad8d4c76ba4836c7d26de37853e36e164a19c
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2019-05-09 17:43:26 -0500

    Updated net_to_mat

--- src/TheseNeedReformulating/Convert_net_to_mat.m
+++ src/TheseNeedReformulating/Convert_net_to_mat.m
@@ -1,18 +1,37 @@
 %% Convert net (network object) to mat
 
 % load network
-file = 'controller_Lcontainer_3in.mat';
-load(file);
-net = netc;
+file = 'controller_Lcontainer_3in.mat'; %name of your file
+%load(file);
 
-% get NN attributes
-W = {net.IW{1} net.LW{2} net.LW{6}};
-b = {net.b{1} net.b{2} net.b{3}};
-activation_fcns = ['relu  ';'relu  ';'linear'];
-layer_sizes = [net.layers{1}.size net.layers{2}.size net.layers{3}.size];
-number_of_inputs = size(net.IW{1},2);
-number_of_outputs = size(W{end},1);
-number_of_layers = length(b);
-% save network
+% Define parameters
+max = 10; %establish max length for the char matrix of activation functions
+
+% Get NN attributes
+% W = {net.IW{1} net.LW{2} net.LW{6}}; % weights
+W{1} = net.IW{1}; % weights layer one
+for i=1:length(net.b)-1 % weights
+    W{i+1} = net.LW{i+1,i};
+end
+
+for i = 1:length(net.b)
+    b{i} = net.b{i}; % bias
+    layer_sizes(i) = net.layers{i}.size; % number of neurons in each layer
+    str = net.layers{i}.transferFcn; % activation function at layer i
+    padded_str = repmat(' ',1, max);
+    padded_str(1:min(20,length(str))) = str(1:min(20,length(str))); % add spaces to have same lengths
+    activation_fcns(i,:) = padded_str; % activation function at layer i with spaces added
+end
+
+% b = {net.b{1} net.b{2} net.b{3}}; % bias
+% activation_fcns = ['relu  ';'relu  ';'linear']; %activation function
+% layer_sizes = [net.layers{1}.size net.layers{2}.size net.layers{3}.size];
+
+% Other parameters
+number_of_inputs = size(net.IW{1},2); % inputs to the NN
+number_of_outputs = size(W{end},1); % outputs to the NN
+number_of_layers = length(b); % number of layers, including hidden and output layers
+
+% Save network
 save(file,'W','b','layer_sizes','activation_fcns','number_of_inputs',...
     'number_of_outputs','number_of_layers');
