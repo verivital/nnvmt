2f913132962bf21198cf1e5e69975260a86732e9
Patrick Musau <patrick.musau@vanderbilt.edu>
2019-07-23 17:23:54 -0500

    added file that does some unit testing

---
+++ test_nnvmt.py
@@ -0,0 +1,9 @@
+import unittest
+from nnvmt import parseArguments
+from nnvmt import decideTool
+from nnvmt import decideOutput
+from nnvmt import parseHandler
+
+#class that implements unit tests for our tool
+class TestNNVMT(unittest.TestCase):
+    pass
