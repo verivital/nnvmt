82fe8462a3cf73cfadee8c4ecfb18ca8fe456986
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-07-26 11:35:15 -0500

    Finished ONNX printer and added examples

--- src/TheseNeedReformulating/ONNX_to_mat(tf).py
+++ src/TheseNeedReformulating/ONNX_to_mat(tf).py
@@ -13,7 +13,8 @@ from onnx_tf.backend import prepare
 import numpy as np
 import scipy.io as sio
 
-#input_path = r'C:\Users\manzand\Documents\MATLAB\ARCH-2019\ACC\controller_3_20.onnx'
+input_path = r'C:\Users\manzand\AnacondaProjects\nnmt\translated_networks\CartPole_Controller.onnx' # Comes from keras
+#input_path = r'C:\Users\manzand\AnacondaProjects\nnmt\ONNX\SingleCarController.onnx' # Comes from matlab
 #output_path = r'ACC_controller_3_20.mat'
 
 # Load ONNX model
@@ -21,6 +22,7 @@ def load_model(input_path):
     onnx_model = onnx.load(input_path)  # load onnx model
     tf_rep = prepare(onnx_model)
     return tf_rep
+model = load_model(input_path)
 
 #Get the dictionary and list of model's tensors
 def inform(model):
@@ -32,6 +34,7 @@ def inform(model):
         temp = [key,value]
         tensor_list.append(temp)
     return tensor_dict,tensor_list
+[td,tl] = inform(model)
 
 # Get weight, bias and activation functions
 def parameters(tensor_dict,tensor_list,model):
@@ -41,20 +44,23 @@ def parameters(tensor_dict,tensor_list,model):
     #empty lists to store weights, biases and act functions
     W = []
     b = []
-    activation_fcns = []
+    act_fcns = []
     # After creating session, we will get the weights, activation layers and bias
     for i in range(len(tensor_dict)):
         if 'W' in tensor_list[i][0]:
-            W.append(np.float64(sess.run(tensor_dict[tensor_list[i][0]])))
+            W.append(np.float64(sess.run(tensor_dict[tensor_list[i][0]]).T))
         elif 'B' in tensor_list[i][0]:
-            b.append(np.float64(sess.run(tensor_dict[tensor_list[i][0]])))
-        elif 'relu' in tensor_list[i][0]:
-            activation_fcns.append('relu')
-        elif 'linear' in tensor_list[i][0]:
-            activation_fcns.append('linear')
-        elif 'tanh' in tensor_list[i][0]:
-            activation_fcns.append('tanh')
-    return W,b,activation_fcns
+            b.append(np.float64(sess.run(tensor_dict[tensor_list[i][0]])).reshape(-1,1))
+        elif 'elu' in tensor_list[i][1].op.name:
+            act_fcns.append('relu')
+        elif 'inear' in tensor_list[i][1].op.name:
+            act_fcns.append('linear')
+        elif 'anh' in tensor_list[i][1].op.name:
+            act_fcns.append('tanh')
+        elif 'igmoid' in tensor_list[i][1].op.name:
+            act_fcns.append('sigmoid')
+    return W,b,act_fcns
+[W,b,act_fcns] = parameters(td,tl,model)
 
 # Get size parameters of neural network
 def net_size(W,b): 
@@ -67,20 +73,29 @@ def net_size(W,b):
     for i in range(len(b)):
         layer_sizes.append(np.int64(b[i].size))
     return number_of_inputs,number_of_outputs,number_of_layers,layer_sizes
+[ni,no,nl,ls] = net_size(W,b)
+
+def reshape(W,b):
+    if len(b[0]) != len(W[0]):
+        for i in range(len(W)):
+            W[i] = W[i].T
+    return W,b
+[w,b] = reshape(W,b)
+        
 
 # Save the nn information in a mat file
-def save_nn_mat_file(ni,no,nls,lsize,W,b,lfs,output_path):
-    nn1 = dict({'number_of_inputs':ni,'number_of_outputs':no ,'number_of_layers':nls,
-                'layer_sizes':lsize,'W':W,'b':b,'activation_fcns':lfs})
+def save_nn_mat_file(W,b,lfs,output_path):
+    nn1 = dict({'W':W,'b':b,'act_fcns':lfs})
     sio.savemat(output_path, nn1)
+#save_nn_mat_file(W,b,lfs,output_path)
 
 # parse the nn imported ONNX with tensorflow backend
 def parse_nn(input_path,output_path):
     model = load_model(input_path)
     [tensor_dict,tensor_list] = inform(model)
     [W,b,lfs] = parameters(tensor_dict,tensor_list,model)
-    [ni,no,nls,lsize] = net_size(W,b)
-    save_nn_mat_file(ni,no,nls,lsize,W,b,lfs,output_path)
+#    [ni,no,nls,lsize] = net_size(W,b)
+    save_nn_mat_file(W,b,lfs,output_path)
 
 #parse_nn(input_path,output_path)
 
