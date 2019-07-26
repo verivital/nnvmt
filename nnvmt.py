27e470f5434775ea198fb7f2b5d0da634925bfde
Patrick Musau <patrick.musau@vanderbilt.edu>
2019-07-26 16:27:56 -0500

    moved many functions to nnvmt_handlers to make code less cluttered

--- nnvmt.py
+++ nnvmt.py
@@ -14,14 +14,9 @@ import os
 import warnings
 os.environ['KMP_WARNINGS'] = 'FALSE'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-from src.reluPlexPrinter import reluplexPrinter
-from src.sherlockPrinter import sherlockPrinter
-from src.kerasPrinter import kerasPrinter
-from src.TensorflowPrinter import TensorflowPrinter
-from src.onnxPrinter import onnxPrinter
-from src.tf_eran_printer import Tf_eran_printer
-from src.nnvmt_exceptions import FileExtensionError
-from src.nnvmt_exceptions import OutputFormatError
+from src.nnvmt_handlers import decideTool
+from src.nnvmt_handlers import decideOutput
+from src.nnvmt_handlers import parseHandler
 #from src.onnxPrinter import onnxPrinter
 
 #function that gets input format from the user
@@ -42,127 +37,6 @@ def commandLineInterface():
     #parses the arguments and stores them in a dictionary
     args=vars(ap.parse_args())
     return args
-
-
-#function that decides which tool the model file is 
-def decideTool(name,inputPath):
-    #get the base name of the path:
-    basename=os.path.basename(inputPath)
-    if (name=="Reluplex") or (name=="reluplex") or (name=="nnet"):
-        #if its from reluplex it will end in .nnet. If not throw an error
-        if('.nnet' in basename):
-            fileType="Reluplex"
-        else:
-            raise FileExtensionError("Error: Unrecognized Neural Network File Format (Kyle Julian 2016). Expected filename extension .nnet")
-    elif name=="Sherlock" or name=="sherlock":
-        #check to see if the input file ends in .txt or nothing
-        if(".txt" in basename or len(basename.split("."))==1): 
-            fileType="Sherlock"
-        else:
-            raise FileExtensionError("Error: Unrecognized Sherlock format. Expected filename extension .txt or nothing")
-    elif name=="Keras" or name=="keras":
-        #check to see if the files provided are correct
-        if('.h5' in basename):
-            fileType="Keras"
-        else:
-            raise FileExtensionError("Error: Unrecognized Keras format. Expected filename extension is .h5")
-    elif name =="Tensorflow" or name == "tensorflow":
-        #check to see if the files provided are correct
-        if('.meta' in basename):
-            fileType="Tensorflow"
-        else:
-            raise FileExtensionError("Error: Unrecognized Tensorflow format. Expected filename extension is .meta")
-    elif name == 'ONNX' or name == 'onnx':
-        #check to see if the provided files are correct
-        if ('.onnx' in basename):
-            fileType = 'ONNX'
-        else:
-            raise FileExtensionError("Error: Unrecognized ONNX format. Expected filename extension is .onnx")
-    elif name=="mat" or name=="Matfile":
-        if('.mat' in basename):
-            fileType="mat"
-        else:
-            raise FileExtensionError("Error: Unrecognized Matfile format. Expected filename extension is .mat")
-    else:
-        raise NameError("Error: Unrecognized input format. Tools currently supported (Reluplex, Sherlock)")
-    return fileType
-
-
-#function that checks if json file is correct
-def checkJson(filepath):
-    if filepath:
-        basename=os.path.basename(filepath)
-        if('.json' in basename):
-            return 1
-        else:
-            return 0
-    else:
-        return 2
-
-
-#function that chooses which printer to use for keras
-def chooseKeras(toolname,printer):
-    if toolname=="mat":
-        printer.create_matfile()
-    elif toolname=="onnx":
-        printer.create_onnx_model()
-#function that performs Keras printing 
-def kerasPrinting(checkNum, inputPath, outputpath, jsonFile,toolname):
-    if(checkNum==2):
-        printer=kerasPrinter(inputPath,outputpath)
-        chooseKeras(toolname,printer)
-    elif(checkNum==1):
-        printer=kerasPrinter(inputPath,outputpath,jsonFile)
-        chooseKeras(toolname,printer)
-    else: 
-        print("Error: Unrecognized Keras Json format. Expected filename extension is .json")
-        printer=None
-    return printer
-    
-#function that decides what the output file should be
-def decideOutput(name):
-    if name=="mat" or name=="Mat" or name=='Matfile' or name=="matfile":
-        outputType="mat"
-    elif name=="onnx" or name=="ONNX":
-        outputType="onnx"
-    elif name=="tf" or name=="ERAN":
-        outputType="tf"
-    else:
-        raise OutputFormatError("Error: Unrecognized output format. Output formats currently supported (Onnx, Mat)")
-    return outputType
-
-#function that hadles the parsing and printing
-def parseHandler(toolName,outputFormat, inputPath, outputpath,jsonFile):
-    if(toolName=="Reluplex" and outputFormat=="mat"):
-        printer=reluplexPrinter(inputPath,outputpath)
-    elif(toolName=="Reluplex" and outputFormat=="onnx"):
-        printer=reluplexPrinter(inputPath,outputpath)
-        printer.create_onnx_model()
-    elif(toolName=="Sherlock" and outputFormat=="mat"):
-        printer=sherlockPrinter(inputPath,outputpath)
-    elif(toolName=="Sherlock" and outputFormat=="onnx"):
-        printer=sherlockPrinter(inputPath,outputpath)
-        printer.create_onnx_model()
-    elif(toolName=="Tensorflow" and outputFormat=="mat"):
-        printer=TensorflowPrinter(inputPath,outputpath,jsonFile)
-    elif(toolName=="ONNX" and outputFormat=='.mat'):
-        printer=onnxPrinter(inputPath,outputpath)
-    elif(toolName=="Keras" and outputFormat=="mat"):
-        #check the json file
-        checkNum=checkJson(jsonFile)
-        printer=kerasPrinting(checkNum, inputPath, outputpath, jsonFile,"mat")
-    elif(toolName=="Keras" and outputFormat=="onnx"):
-        checkNum=checkJson(jsonFile)
-        printer=kerasPrinting(checkNum, inputPath, outputpath, jsonFile,"onnx")
-        #raise NotImplementedError("Sorry. Still developping Keras to Onnx printer.")
-    elif (toolName=="mat" and outputFormat=="tf"):
-        printer=Tf_eran_printer(inputPath,outputpath)
-    else:
-        print(toolName,outputFormat)
-        printer=None
-    return printer
-        
-    
 
 #parse aguments
 def parseArguments(arguments):
